31ece596c1f941f51fbe8c208549cfa494ba4b77
Adrian Melgar <DaikiMT@outlook.es>
2023-11-12 16:13:18 -0500

    Implemented AppDbContext

--- nourishify.api/Shared/Infrastructure/Persistence/Configuration/AppDbContext.cs
+++ nourishify.api/Shared/Infrastructure/Persistence/Configuration/AppDbContext.cs
@@ -1,6 +1,37 @@
+using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
+using Microsoft.EntityFrameworkCore;
+using nourishify.api.Shared.Infrastructure.Persistence.Configuration.Extensions;
+
 namespace nourishify.api.Shared.Infrastructure.Persistence.Configuration;
 
-public class AppDbContext
+/**
+ * <summary>
+ *     Application Database Context
+ *
+ *     This class is responsible for configuring the database context for the application. 
+ * </summary>
+ */
+public class AppDbContext : DbContext
 {
-    
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        // Enable Created/Updated Interceptors
+        builder.AddCreatedUpdatedInterceptor();
+        
+        base.OnConfiguring(builder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+       
+        // Apply snake case naming convention
+        builder.UseSnakeCaseNamingConvention();
+    }
 }
