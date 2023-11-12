ce5631e314aafee1bee6f59e4edea8127c6f634d
Adrian Melgar <DaikiMT@outlook.es>
2023-11-12 16:10:55 -0500

    Implemented ModelStateExtensions

--- nourishify.api/Shared/Infrastructure/Persistence/Configuration/Extensions/ModelStateExtensions.cs
+++ nourishify.api/Shared/Infrastructure/Persistence/Configuration/Extensions/ModelStateExtensions.cs
@@ -1,6 +1,14 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 namespace nourishify.api.Shared.Infrastructure.Persistence.Configuration.Extensions;
 
-public class ModelStateExtensions
+public static class ModelStateExtensions
 {
+    public static List<string> GetErrorMessages(this ModelStateDictionary dictionary)
+    {
+        return dictionary.SelectMany(m => m.Value!.Errors)
+            .Select(m => m.ErrorMessage)
+            .ToList();
+    }
     
 }
