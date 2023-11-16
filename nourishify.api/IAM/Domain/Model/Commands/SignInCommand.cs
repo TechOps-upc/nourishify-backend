4a36a3293a102f0d704821f1f4d1e4b49ae5b4d3
Adrian Melgar <DaikiMT@outlook.es>
2023-11-15 19:53:41 -0500

    Added commands

---
+++ nourishify.api/IAM/Domain/Model/Commands/SignInCommand.cs
@@ -0,0 +1,3 @@
+namespace nourishify.api.IAM.Domain.Model.Commands;
+
+public record SignInCommand(string Email, string Password);
---
+++ nourishify.api/IAM/Domain/Model/Commands/SignUpCommand.cs
@@ -0,0 +1,3 @@
+namespace nourishify.api.IAM.Domain.Model.Commands;
+
+public record SignUpCommand(string FirstName, string LastName, string Email, string Password);
