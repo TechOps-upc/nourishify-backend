197949bf3fd53b4530f244daa8947c8351c41a12
Adrian Melgar <DaikiMT@outlook.es>
2023-11-16 18:00:14 -0500

    Implemented basic functions for the user controller

--- nourishify.api/IAM/Interfaces/UsersController.cs
+++ nourishify.api/IAM/Interfaces/UsersController.cs
@@ -1,8 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
+using nourishify.api.IAM.Domain.Model.Queries;
+using nourishify.api.IAM.Domain.Services;
+using nourishify.api.IAM.Interfaces.REST.Transform;
 
 namespace nourishify.api.IAM.Interfaces;
 
 public class UsersController : ControllerBase
 {
+    private readonly IUserQueryService _userQueryService;
     
+    public UsersController(IUserQueryService userQueryService)
+    {
+        _userQueryService = userQueryService;
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var getUserByIdQuery = new GetUserByIdQuery(id);
+        var user = await _userQueryService.Handle(getUserByIdQuery);
+        var userResource = UserResourceFromEntityAssembler.ToResourceFromEntity(user!);
+        return Ok(userResource);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllUsers()
+    {
+        var getAllUsersQuery = new GetAllUsersQuery();
+        var users = await _userQueryService.Handle(getAllUsersQuery);
+        var userResources = users
+            .Select(UserResourceFromEntityAssembler.ToResourceFromEntity);
+        return Ok(userResources);
+    }
 }
