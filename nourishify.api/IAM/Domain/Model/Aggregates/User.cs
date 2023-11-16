53a48222712da45df6802c1012d2b4f72e185b34
Adrian Melgar <DaikiMT@outlook.es>
2023-11-15 19:48:13 -0500

    Implemented hashing password to the user aggregate

--- nourishify.api/IAM/Domain/Model/Aggregates/User.cs
+++ nourishify.api/IAM/Domain/Model/Aggregates/User.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using nourishify.api.IAM.Domain.Model.ValueObjects;
 
 namespace nourishify.api.IAM.Domain.Model.Aggregates;
@@ -23,7 +24,59 @@ public class User
     public long Id { get; set; }
     public PersonName Name { get; private set; }
     public EmailAddress Email { get; private set; }
-    public string PasswordHash { get; private set; }
+    [JsonIgnore] public string PasswordHash { get; private set; }
     public string Username { get; private set; }
     
+    /**
+     * <summary>
+     *     Updates the username of the user.
+     * </summary>
+     * <param name="username">The new username.</param>
+     * <returns>The updated user.</returns>
+     */
+    public User UpdateUsername(string username)
+    {
+        Username = username;
+        return this;
+    }
+    
+    /**
+     * <summary>
+     *     Updates the password hash of the user.
+     * </summary>
+     * <param name="passwordHash">The new password hash.</param>
+     * <returns>The updated user.</returns>
+     */
+    public User UpdatePasswordHash(string passwordHash)
+    {
+        PasswordHash = passwordHash;
+        return this;
+    }
+    
+    /**
+     * <summary>
+     *    Updates the name of the user.
+     * </summary>
+     * <param name="firstName">The new first name.</param>
+     * <param name="lastName">The new last name.</param>
+     * <returns>The updated user.</returns>
+     */
+    public User UpdateName(string firstName, string lastName)
+    {
+        Name = new PersonName(firstName, lastName);
+        return this;
+    }
+    
+    /**
+     * <summary>
+     *    Updates the email of the user.
+     * </summary>
+     * <param name="email">The new email.</param>
+     * <returns>The updated user.</returns>
+     */
+    public User UpdateEmail(string email)
+    {
+        Email = new EmailAddress(email);
+        return this;
+    }
 }
